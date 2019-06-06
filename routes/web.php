b14365d138723f8ca7d4a2d9c4d0f14c69a351cb
Matheus <matheusfcc2010@hotmail.com>
2019-06-05 21:26:05 -0300

    *rota de pagina inicial

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', function() {
     return view('home');
 });
+
+// Route::get('/home', function() {
+//     return view('home');
+// });
 
