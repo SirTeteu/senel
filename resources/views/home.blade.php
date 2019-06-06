59bf1909992a258c4f087727771ce813d7d36a86
Matheus <matheusfcc2010@hotmail.com>
2019-06-05 21:04:22 -0300

    +main section, +about section

---
+++ resources/views/home.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>SENEL - Semana da Engenharia Elétrica UFRJ</title>
+
+		<!--Imports do css do Materialize-->
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+		<link type="text/css" rel="stylesheet" href="css/home.css">
+	</head>
+
+	<body>
+		<div class="first-section">
+			<nav class="transparent z-depth-0">
+				<ul class="hide-on-med-and-down">
+					<li><a class="yellow-text" href="sass.html">Página Inicial</a></li>
+					<li><a href="badges.html">Galeria</a></li>
+					<li><a href="collapsible.html">Edições anteriores</a></li>
+					<li><a href="aladdin.html">Contato</a></li>
+				</ul>
+			</nav>
+			<img src="assets/logo/logo_completa.png">
+		</div>
+
+		<section class="about">
+			<h6>SOBRE</h6>
+			<p>
+				<span>A Semana da Engenharia Elétrica é um evento</span>
+				<span>organizado por alunos do Departamento de</span>
+				<span>Engenharia Elétrica da UFRJ, sem fins lucrativos, que</span>
+				<span>promove palestras, minicursos, painéis, visitas</span>
+				<span>técnicas e workshops, que visam formentar o</span>
+				<span>conhecimento além da sala de aula, e ainda</span>
+				<span>despertar nos alunos o interesse pelo estudo e</span>
+				<span>conhecimento mais próximo da engenharia.</span>
+			</p>
+		</section>
+
+		<!-- Import do js do Materialize -->
+		<script type="text/javascript" src="js/materialize.min.js"></script>
+	</body>
+</html>
